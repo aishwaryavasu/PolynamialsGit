05edad3ef6a4cf921d7c4c5cbb627de17c7032fa
aishwaryavasu <aishwaryavasu1997@gmail.com>
2018-08-07 12:05:33 +0530

    polynomial parse

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,16 @@
 public class MainClass {
-    Polynomial p =new Polynomial("-3x^5 - 4x - 4x^2 + 6");
-    p.print();
+    public static void main(String args[]) {
+        poly p = new poly("x^5 + 3x^3 + 4");
+        poly q = new poly("6x^6 + 4x^3");
+        q.add(p).printPoly();
+        System.out.println();
+        q.sub(p).printPoly();
+        System.out.println();
+        q.multiply(p).printPoly();
+        System.out.println();
+        System.out.print("Remainder\n");
+        q.multiply(p).divide(q).printPoly();
+        System.out.println();
+
+    }
 }
--- src/poly.java
+++ src/poly.java
@@ -1,92 +1,201 @@
+import java.util.ArrayList;
 
 public class poly {
 
-	
-	public static void print(int[] temp)
-	{
-		for(int i=0;i<temp.length;i++)
-		{
-			System.out.print(temp[i]+" ");
-		}
+	int[] coeffecients;
+
+	public poly(){
+		coeffecients=new int[10];
 	}
-	
-	public static int find_length(int[] a ,int[] b)
+	public poly(String polynomial){
+
+
+		ArrayList<Integer> coeffPoly=new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			coeffPoly.add(0);
+		}
+		polynomial=polynomial.replaceAll("\\-","+-");
+
+		polynomial=polynomial.replaceAll("\\s","");
+		if(polynomial.charAt(0)=='+'){
+			polynomial=polynomial.substring(1);
+		}
+		String[] terms=polynomial.split("\\+");
+		for(int i=0;i<terms.length;i++){
+			int power;int coeff=0;
+
+			String[] exp=terms[i].split("\\^");
+			if(exp.length>1){
+				power=Integer.parseInt(exp[1]);
+			}
+			else if(terms[i].indexOf('x')!=-1){
+				power=1;
+			}
+			else {power=0;}
+
+			if(exp[0].equals("x")){
+				coeff=1;
+			}
+			else{
+
+				String[] findCoeff=exp[0].split("x");
+				if(findCoeff[0].equals("-"))coeff=-1;
+				else {
+					coeff=Integer.parseInt(findCoeff[0]);
+				}
+			}
+
+			coeffPoly.set(power,coeff);
+		}
+		coeffecients = new int[coeffPoly.size()];
+		for(int i =0 ; i<coeffPoly.size();i++){
+			coeffecients[i]=coeffPoly.get(i);
+		}
+
+	}
+	public poly(int[] coeffecient) {
+		super();
+		this.coeffecients = new int[ coeffecient.length ];
+		for( int i = 0; i < coeffecient.length; i++ ){
+			this.coeffecients[ i ] = coeffecient[ i ];
+		}
+
+	}
+	public poly(int coeffecient,int degree) {
+		super();
+		coeffecients = new int[degree+1];
+		coeffecients[degree]=coeffecient;
+	}
+	public poly( poly p ){
+		coeffecients = new int[ p.coeffecients.length ];
+		for( int i = 0; i < p.coeffecients.length; i++ ){
+			coeffecients[ i ] = p.coeffecients[ i ];
+		}
+
+	}
+	public int degree(){
+		int d = -1;
+		for( int i = 0; i < coeffecients.length; i++ )
+			if( coeffecients[ i ] != 0 ) d = i;
+		return d;
+	}
+
+	public void printPoly()
 	{
-		int len = b.length;
-		if(len<a.length)
-			len=a.length;
+		if(toString().startsWith("+"))
+		System.out.println(toString().substring(1));
+		else
+			System.out.println(toString());
+
+	}
+
+	public String toString() {
+		String st = "";
+		int degree = degree();
+		for (int i = coeffecients.length - 1; i >= 0; i--) {
+			if (coeffecients[i] != 0) {
+				if (coeffecients[i] < 0)
+					st += coeffecients[i];
+				else
+					st += "+" + coeffecients[i];
+				if (i > 0) {
+					st += "X";
+					if (i > 1) {
+						st += "^" + i + " ";
+					}
+
+				}
+			}
+		}	return st;
+
+	}
+	public int findHigherDegree(poly b)
+	{
+		int len = this.degree();
+		if(len<b.degree())
+			len=this.degree();
 		return len;
 	}
-	
-	public static  void add (int[] a, int[] b)
-	{
-		int len=find_length(a,b);
-		int temp[] = new int[len];
-		
-		
-		for(int i=len-1;i>=0;i--)
-		{
-			int temp_1 = a[i]+b[i];
-			temp[i]=temp[i]+temp_1;//+(temp_1%10);
-			//if(temp_1>9)
-			//{
-			//	temp_1 = temp_1/10;
-		//		temp[i]=temp[i]+temp_1;
-		//	}
-		}
-		print(temp);
-		
-		
-		
+	public int getCoeff(int n){
+		if(n<=degree())
+		return coeffecients[n];
+		return 0;
 	}
 	
-	public static void sub(int[] a,int[] b)
+	public  poly add (poly b)
 	{
-		int len=find_length(a,b);
-		int temp[] = new int[len];
-		
-		
-		for(int i=len-1;i>=0;i--)
+		int len=this.findHigherDegree(b);
+		int temp[] = new int[len+1];
+
+
+		for(int i=len;i>=0;i--)
 		{
-			int temp_1 = a[i]-b[i];
+			int temp_1 =0;
+			temp_1 = this.getCoeff(i) + b.getCoeff(i);
 			temp[i]=temp_1;
-			
 		}
-		print(temp);
+		return new poly(temp);
+		
 	}
 	
-	public static void multiply(int[] a,int[] b)
+	public poly sub(poly b)
 	{
-		int len= find_length(a,b);
-		int temp[] = new int[2*len-1];
-		
-		for(int i=len-1;i>=0;i--)
+		int len=this.findHigherDegree(b);
+		int temp[] = new int[len+1];
+
+
+		for(int i=len;i>=0;i--)
 		{
-			for(int j=len-1;j>=0;j--)
+			int temp_1 =0;
+			if(b.degree()>i && this.degree()>i)
+				temp_1 = this.getCoeff(i) - b.getCoeff(i);
+			else if(b.degree()>i)
+				temp_1 = b.getCoeff(i);
+			else if(this.degree()>i)
+				temp_1=this.getCoeff(i);
+			temp[i]=temp_1;
+		}
+
+		return new poly(temp);
+	}
+	
+	public poly multiply(poly b)
+	{
+		int len= this.findHigherDegree(b);
+		int temp[] = new int[this.degree()+b.degree()+1];
+		
+		for(int i=this.degree();i>=0;i--)
+		{
+			for(int j=b.degree();j>=0;j--)
 			{
-				temp[i+j]=temp[i+j]+(a[i]*b[j]);
+				temp[i+j]=temp[i+j]+(this.getCoeff(i)*b.getCoeff(j));
 			}
 		}
-		print(temp);
+		return new poly(temp);
+
 	}
-	
-	
-	/*public static void main(String[] args)
-	{
-		int[] a = {1,2,3,4};
-		int[] b = {9,6,7,8};
-		
-		
-		add(a,b);
-		System.out.println();
-		sub(a,b);
-		System.out.println();
-		multiply(a,b);
-		
-	}*/
-	
-	
-	
-	
-	
+	public poly divide(poly divisor){
+		poly quotient = new poly();
+		String q ="";
+		poly remainder = new poly(this);
+		int i=0;
+		while(!remainder.isZero() && remainder.degree()>=divisor.degree()){
+			int coef = remainder.getCoeff(remainder.degree()) / divisor.getCoeff(divisor.degree());
+			int deg = remainder.degree() - divisor.degree();
+			poly temp = new poly( coef, deg );
+			q+=temp.toString();
+			quotient = temp.add( quotient );
+			remainder = remainder.sub( temp.multiply( divisor ) );
+
+		}
+		System.out.println("quotient " +q);
+		return remainder;
+	}
+	public boolean isZero(){
+		for( int i : coeffecients ){
+			if( i != 0 ) return false;
+		}
+		return true;
+	}
+
 }
